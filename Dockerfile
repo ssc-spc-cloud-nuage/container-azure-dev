f96a79c2085c9ca474d1ba6ce92ced3757238519
Bernard Maltais <bernard.maltais@canada.ca>
2021-06-28 11:10:12 -0400

    Updating to terraform v1.0.1

--- Dockerfile
+++ Dockerfile
@@ -4,51 +4,40 @@
 #-------------------------------------------------------------------------------------------------------------
 
 # Pick any base image, but if you select node, skip installing node. 😊
-FROM mcr.microsoft.com/vscode/devcontainers/base:ubuntu-18.04
-# FROM ubuntu:18.04
+FROM mcr.microsoft.com/vscode/devcontainers/base:0-buster
 
 # Avoid warnings by switching to noninteractive
 ENV DEBIAN_FRONTEND=noninteractive
 
 # Terraform and tflint versions
-ARG TERRAFORM_VERSION=0.12.31
-ARG TFLINT_VERSION=0.28.1
+ARG TERRAFORM_VERSION=1.0.1
+ARG TFLINT_VERSION=0.29.1
+ARG TERRAGRUNT_VERSION=0.31.0
 
-# This Dockerfile adds a non-root user with sudo access. Use the "remoteUser"
-# property in devcontainer.json to use it. On Linux, the container user's GID/UIDs
-# will be updated to match your local UID/GID (when using the dockerFile property).
-# See https://aka.ms/vscode-remote/containers/non-root-user for details.
-ARG USERNAME=vscode
-ARG USER_UID=1000
-ARG USER_GID=$USER_UID
-# [Option] Install Powershell 7
-ARG INSTALL_POWERSHELL="false"
+FROM mcr.microsoft.com/vscode/devcontainers/base:0-buster
 
 COPY library-scripts/*.sh /tmp/library-scripts/
 
+# [Option] Install zsh
+ARG INSTALL_ZSH="true"
+# [Option] Upgrade OS packages to their latest versions
+ARG UPGRADE_PACKAGES="false"
+
+# Install needed packages and setup non-root user. Use a separate RUN statement to add your own dependencies.
+ARG USERNAME=vscode
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+
+RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
+    && bash /tmp/library-scripts/common-debian.sh "${INSTALL_ZSH}" "${USERNAME}" "${USER_UID}" "${USER_GID}" "${UPGRADE_PACKAGES}" "true" "true" \ 
+    && apt-get install -y graphviz \
+    && apt-get clean -y && rm -rf /var/lib/apt/lists/*
+
 # Configure apt and install packages
 RUN apt-get update \
-    && /bin/bash /tmp/library-scripts/common-debian.sh "${INSTALL_ZSH}" "${USERNAME}" "${USER_UID}" "${USER_GID}" "${UPGRADE_PACKAGES}" \
     && bash /tmp/library-scripts/azcli-debian.sh \
     # Terraform, tflint
-    && bash /tmp/library-scripts/terraform-debian.sh "${TERRAFORM_VERSION}" "${TFLINT_VERSION}" \
-    #
-    # Install graphviz
-    #
-    # && apt-get install -y graphviz \
-    #
-    # Install tmux
-    #
-    # && apt-get install -y tmux \
-    #
-    # Clean up
-    && apt-get autoremove -y \
-    && apt-get clean -y \
-    && rm -rf /var/lib/apt/lists/*
-
-    # Powershell 7
-RUN if [ "${INSTALL_POWERSHELL}" = "true" ]; then bash /tmp/library-scripts/powershell-debian.sh; fi \
-    #
+    && bash /tmp/library-scripts/terraform-debian.sh "${TERRAFORM_VERSION}" "${TFLINT_VERSION}" "${TERRAGRUNT_VERSION}" \
     # Clean up
     && apt-get autoremove -y \
     && apt-get clean -y \
@@ -58,3 +47,5 @@ RUN if [ "${INSTALL_POWERSHELL}" = "true" ]; then bash /tmp/library-scripts/powe
 USER $USERNAME
 WORKDIR /home/$USERNAME
 ENV DEBIAN_FRONTEND=dialog
+
+CMD [ "sleep", "infinity" ]
--- library-scripts/terraform-debian.sh
+++ library-scripts/terraform-debian.sh
@@ -3,21 +3,20 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
+#
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/terraform.md
+# Maintainer: The VS Code and Codespaces Teams
+#
+# Syntax: ./terraform-debian.sh [terraform version] [tflint version] [terragrunt version]
 
-# Syntax: ./terraform-debian.sh <terraform version> [tflint version]
-
-TERRAFORM_VERSION=$1
-TFLINT_VERSION=${2:-"none"}
+TERRAFORM_VERSION=${1:-"latest"}
+TFLINT_VERSION=${2:-"latest"}
+TERRAGRUNT_VERSION=${3:-"latest"}
 
 set -e
 
-if [ -z "$1" ]; then
-    echo -e "Required argument missing.\n\nterraform-debian.sh <terraform version> [tflint version]"
-    exit 1
-fi
-
 if [ "$(id -u)" -ne 0 ]; then
-    echo -e 'Script must be run a root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
     exit 1
 fi
 
@@ -30,7 +29,26 @@ if ! dpkg -s curl ca-certificates unzip > /dev/null 2>&1; then
     apt-get -y install --no-install-recommends curl ca-certificates unzip
 fi
 
-# Install Terraform, tflint
+
+if [ "${TERRAFORM_VERSION}" = "latest" ] || [ "${TERRAFORM_VERSION}" = "lts" ] || [ "${TERRAFORM_VERSION}" = "current" ]; then
+    TERRAFORM_VERSION=$(curl -sSL https://releases.hashicorp.com/terraform/ | grep -m1 -oE '>terraform_[0-9]+\.[0-9]+\.[0-9]+<' | sed 's/^>terraform_\(.*\)<$/\1/')
+fi
+
+if [ "${TFLINT_VERSION}" = "latest" ] || [ "${TFLINT_VERSION}" = "lts" ] || [ "${TFLINT_VERSION}" = "current" ]; then
+    TFLINT_VERSION=$(basename "$(curl -fsSL -o /dev/null -w "%{url_effective}" https://github.com/terraform-linters/tflint/releases/latest)")
+fi
+if [ "${TFLINT_VERSION::1}" != 'v' ]; then
+    TFLINT_VERSION="v${TFLINT_VERSION}"
+fi
+
+if [ "${TERRAGRUNT_VERSION}" = "latest" ] || [ "${TERRAGRUNT_VERSION}" = "lts" ] || [ "${TERRAGRUNT_VERSION}" = "current" ]; then
+    TERRAGRUNT_VERSION=$(basename "$(curl -fsSL -o /dev/null -w "%{url_effective}" https://github.com/gruntwork-io/terragrunt/releases/latest)")
+fi
+if [ "${TERRAGRUNT_VERSION::1}" != 'v' ]; then
+    TERRAGRUNT_VERSION="v${TERRAGRUNT_VERSION}"
+fi
+
+# Install Terraform, tflint, Terragrunt
 echo "Downloading terraform..."
 mkdir -p /tmp/tf-downloads
 curl -sSL -o /tmp/tf-downloads/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
@@ -39,9 +57,16 @@ mv -f terraform /usr/local/bin/
 
 if [ "${TFLINT_VERSION}" != "none" ]; then
     echo "Downloading tflint..."
-    curl -sSL -o /tmp/tf-downloads/tflint.zip https://github.com/terraform-linters/tflint/releases/download/v${TFLINT_VERSION}/tflint_linux_amd64.zip
+    curl -sSL -o /tmp/tf-downloads/tflint.zip https://github.com/terraform-linters/tflint/releases/download/${TFLINT_VERSION}/tflint_linux_amd64.zip
     unzip /tmp/tf-downloads/tflint.zip
     mv -f tflint /usr/local/bin/
+fi
+
+if [ "${TERRAGRUNT_VERSION}" != "none" ]; then
+    echo "Downloading Terragrunt..."
+    curl -sSL -o /tmp/tf-downloads/terragrunt https://github.com/gruntwork-io/terragrunt/releases/download/${TERRAGRUNT_VERSION}/terragrunt_linux_amd64
+    chmod a+x /tmp/tf-downloads/terragrunt
+    mv -f /tmp/tf-downloads/terragrunt /usr/local/bin/
 fi
 
 rm -rf /tmp/tf-downloads
